cae0083debaa2be50935e6e4417e11e97d5caa24
duongbaduy <riox.doubled@gmail.com>
2024-05-13 10:13:54 +0700

    Xoa tk, lien hệ...

--- contact-us.php
+++ contact-us.php
@@ -97,41 +97,6 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12 col-custom">
-                    <form method="post" action="http://whizthemes.com/mail-php/reza/flosun/mail.php" id="contact-form" accept-charset="UTF-8" class="contact-form">
-                        <div class="comment-box mt-5">
-                            <h5 class="text-uppercase"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">GÓP Ý VỀ SHOP</font></font></h5>
-                            <div class="row mt-3">
-                                <div class="col-md-6 col-custom">
-                                    <div class="input-item mb-4">
-                                        <input class="border-0 rounded-0 w-100 input-area name gray-bg" type="text" name="con_name" id="con_name" placeholder="Tên">
-                                    </div>
-                                </div>
-                                <div class="col-md-6 col-custom">
-                                    <div class="input-item mb-4">
-                                        <input class="border-0 rounded-0 w-100 input-area email gray-bg" type="email" name="con_email" id="con_email" placeholder="E-mail">
-                                    </div>
-                                </div>
-                                <div class="col-12 col-custom">
-                                    <div class="input-item mb-4">
-                                        <input class="border-0 rounded-0 w-100 input-area email gray-bg" type="text" name="con_content" id="con_content" placeholder="Chủ thể">
-                                    </div>
-                                </div>
-                                <div class="col-12 col-custom">
-                                    <div class="input-item mb-4">
-                                        <textarea cols="30" rows="5" class="border-0 rounded-0 w-100 custom-textarea input-area gray-bg" name="con_message" id="con_message" placeholder="Tin nhắn"></textarea>
-                                    </div>
-                                </div>
-                                <div class="col-12 col-custom mt-40">
-                                    <button type="submit" id="submit" name="submit" class="btn flosun-button secondary-btn theme-color rounded-0"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Gửi tin nhắn</font></font></button>
-                                </div>
-                                <p class="col-8 col-custom form-message mb-0"></p>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
         </div>
     </div>
     <!-- Contact Us Area End Here -->
